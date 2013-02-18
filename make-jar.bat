eace9630a751b5883c902c3fe9be9cc4f09812a8
Peter Brown <phbrown@acm.org>
2013-02-18 13:49:58 -0500

    Import from Bazaar to git

--- make-jar.bat
+++ make-jar.bat
@@ -1,10 +1,10 @@
-set java_root="C:\Program Files\Java\jdk1.6.0_18"
+set java_root="C:\Program Files\Java\jdk1.7.0_03"
 if not exist %java_root% set java_root="C:\Program Files\Java\jdk1.6.0_04"
 
 set dest="..\jar-stuff"
 if not exist %dest% set dest="..\..\jar-stuff"
 
-%java_root%\bin\jar.exe cmf manifest.txt %dest%\OrbitalVis.jar *.java *.class *.gif
+%java_root%\bin\jar.exe cmf manifest.txt OrbitalVis.jar *.java *.class *.gif
 copy *.txt %dest%
 copy *.dll %dest%
 copy *.jar %dest%
