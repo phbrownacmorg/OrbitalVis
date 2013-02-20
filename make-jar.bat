7aa723a691c8f90f198c76d6b76206f2c3e3c5b9
Peter Brown <phbrown@acm.org>
2013-02-20 10:43:09 -0500

    Made the code compile
    Added the proper classpath to make-jar.bat
    Added junit-4.11.jar to support unit tests
    Added .gitignore to ignore backup files and .class files

--- make-jar.bat
+++ make-jar.bat
@@ -4,6 +4,7 @@ if not exist %java_root% set java_root="C:\Program Files\Java\jdk1.6.0_04"
 set dest="..\jar-stuff"
 if not exist %dest% set dest="..\..\jar-stuff"
 
+%java_root%\bin\javac.exe -classpath jogl.jar;junit-4.11.jar *.java
 %java_root%\bin\jar.exe cmf manifest.txt OrbitalVis.jar *.java *.class *.gif
 copy *.txt %dest%
 copy *.dll %dest%
