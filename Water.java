efb63f83be6808dbf8c063115e99b174fe91b30c
Clay Hamilton <claytonthamilton@gmail.com>
2013-02-27 13:57:50 -0500

    Put water into Sn2 reaction instead of OH, incomplete

--- Water.java
+++ Water.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Class to model a hydroxide group.  The origin of the oxygen atom is 
  * treated as the origin of the group.
@@ -40,4 +41,45 @@ public class Water extends AtomOrGroup {
     SP3AtomView oxyView = oxygen.createView("", AtomView.O_RED);
     return new WaterView(this, h1View, h2View, oxyView, text);
   }
+=======
+/**
+ * Class to model a hydroxide group.  The origin of the oxygen atom is 
+ * treated as the origin of the group.
+ *
+ * Copyright 2010 Peter Brown <phbrown@acm.org> and Madonna King
+ *
+ * This code is distributed under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * license or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public class Water extends AtomOrGroup {
+  private Atom h1, h2;
+  private SP3Atom oxygen;
+  
+  public Water(Point3D pt, AtomOrGroup.Charge charge) {
+    super(pt, charge);
+    
+    Point3D hydroLoc = new Point3D(0, 0, Atom.S_TO_SP3_BOND_LENGTH);
+    Matrix rX= Matrix.makeRotationMatrix(109.5, Matrix.Axis.X);
+    Matrix rZ = Matrix.makeRotationMatrix(120, Matrix.Axis.Z);
+    Matrix rotMatrix = rZ.mult(rX);
+    hydroLoc = hydroLoc.transform(rotMatrix);
+    h1 = new Atom(hydroLoc);
+    hydroLoc = new Point3D(0, 0, Atom.S_TO_SP3_BOND_LENGTH);
+    rotMatrix = Matrix.makeRotationMatrix(109.5, Matrix.Axis.X);
+    hydroLoc = hydroLoc.transform(rotMatrix);
+    h2 = new Atom(hydroLoc);
+    oxygen = new SP3Atom();
+  }
+  
+  public WaterView createView(String text) {
+    HydrogenView h1View = (HydrogenView)(h1.createView());
+    HydrogenView h2View = (HydrogenView)(h2.createView());
+    SP3AtomView oxyView = oxygen.createView("", AtomView.O_RED);
+    return new WaterView(this, h1View, h2View, oxyView, text);
+  }
+>>>>>>> Put water into Sn2 reaction instead of OH, incomplete
 }
--- WaterView.java
+++ WaterView.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import javax.media.opengl.*;
  
 /** 
@@ -38,3 +39,45 @@ public class WaterView extends AtomOrGroupView {
     endDraw(gl);
   }
 }
+=======
+import javax.media.opengl.*;
+ 
+/** 
+ * A class to view a hydroxide group.  The oxygen is 
+ * treated as the origin of the group.
+ *
+ * Copyright 2010 Peter Brown <phbrown@acm.org> and Madonna King
+ *
+ * This code is distributed under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the
+ * license or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class WaterView extends AtomOrGroupView {
+  public static final String TEXT_OH = "OH";
+  public static final String TEXT_HO = "HO";
+  
+  private HydrogenView h1View;
+  private HydrogenView h2View;
+  private SP3AtomView oxyView;
+  
+  public WaterView(Water frame, HydrogenView h1View, HydrogenView h2View, SP3AtomView oview, String text) {
+    super(frame, text);
+    this.h1View=h1View;
+    this.h2View=h2View;
+    oxyView = oview;
+  }
+  
+  public void draw(GL gl) {
+    initDraw(gl);
+    //ShapeBuilder.axes(gl);
+    oxyView.draw(gl);
+    h1View.draw(gl);
+    h2View.draw(gl);
+    endDraw(gl);
+  }
+}
+>>>>>>> Put water into Sn2 reaction instead of OH, incomplete
